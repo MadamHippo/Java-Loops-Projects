2af0ba22aaa16ed13426f71a4e6b9ebcc475a993
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-10-02 02:22:57 -0700

    Update single-out-digits-mod.java

--- single-out-digits-mod.java
+++ single-out-digits-mod.java
@@ -39,7 +39,8 @@ public class BurritoCalculator {
     
   
   
-  
+  /* Solution explained:
+There’s a couple of things to take note of here. The first is that we use % 10 to find the digit in the ones place. For example, 73 % 10 will give you 3. After we find the ones digit, we compare that digit to our previous maximum and update that maximum accordingly. Finally, we look at the next digit by dividing by 10. This will essentially chop off the last digit — 73 / 10 will give us 7 — the 3 has been chopped off! We’ll keep doing this until we chop off the last digit. What does it mean to chop off the last digit? Well if we divide 7 by 10, we’ll end up with 0. So we know we have digits to cut off as long as budget > 0. */
   
   public static void main(String []args) {
     // Try passing different arguments for your budget with varying numbers of digits, like 62, 374, 599, etc
